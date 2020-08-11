9fb11c4768640016edb3611211e6c3f220f0ced4
the6p4c <me@doggirl.gay>
2020-08-11 20:26:41 +1000

    Add test for mapping pass

--- src/flowmap/map.rs
+++ src/flowmap/map.rs
@@ -1,6 +1,7 @@
 use super::*;
 use std::collections::HashSet;
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct LUT<Ni: NodeIndex> {
     /// The node that the LUT generates.
     pub output: Ni,
@@ -162,5 +163,71 @@ mod tests {
             },
             vec![6, 7, 9]
         );
+    }
+
+    #[test]
+    fn map_test() {
+        // Fig. 5(a) from FlowMap paper, numbered top-to-bottom, left-to-right.
+        let mut network = FlowMapBooleanNetwork::<usize>::new(12);
+
+        network.add_edge(From(0), To(5));
+        network.add_edge(From(1), To(5));
+        network.add_edge(From(1), To(6));
+        network.add_edge(From(2), To(6));
+        network.add_edge(From(3), To(7));
+        network.add_edge(From(4), To(7));
+        network.add_edge(From(5), To(8));
+        network.add_edge(From(5), To(12));
+        network.add_edge(From(6), To(8));
+        network.add_edge(From(6), To(10));
+        network.add_edge(From(7), To(9));
+        network.add_edge(From(7), To(11));
+        network.add_edge(From(8), To(9));
+        network.add_edge(From(9), To(10));
+        network.add_edge(From(10), To(11));
+        network.add_edge(From(11), To(12));
+
+        // Mark PI nodes
+        network.node_value_mut(0).is_pi = true;
+        network.node_value_mut(1).is_pi = true;
+        network.node_value_mut(2).is_pi = true;
+        network.node_value_mut(3).is_pi = true;
+        network.node_value_mut(4).is_pi = true;
+
+        // Mark PO node
+        network.node_value_mut(12).is_po = true;
+
+        // Give nodes their correct \bar{X} set
+        // We'll skip the nodes which shouldn't have LUTs generated for them, as
+        // we should never need to access those sets.
+        network.node_value_mut(5).x_bar = vec![5];
+        network.node_value_mut(6).x_bar = vec![6];
+        network.node_value_mut(7).x_bar = vec![7];
+        network.node_value_mut(12).x_bar = vec![8, 9, 10, 11, 12];
+
+        let luts = map(&network, 3);
+
+        // As per Fig. 4 (c), we should end up with 4 LUTs
+        assert_eq!(luts.len(), 4);
+        assert!(luts.contains(&LUT {
+            output: 5,
+            inputs: vec![0, 1],
+            contains: vec![5],
+        }));
+        assert!(luts.contains(&LUT {
+            output: 6,
+            inputs: vec![1, 2],
+            contains: vec![6],
+        }));
+        assert!(luts.contains(&LUT {
+            output: 7,
+            inputs: vec![3, 4],
+            contains: vec![7],
+        }));
+        assert!(luts.contains(&LUT {
+            output: 12,
+            inputs: vec![5, 6, 7],
+            contains: vec![8, 9, 10, 11, 12],
+        }));
     }
 }
